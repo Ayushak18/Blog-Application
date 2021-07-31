c8cabfc7c880b73253f0280b97230b2a2a911930
Ayushak18 <aayushkamboj7@gmail.com>
2021-07-31 19:27:05 +0530

    Pagination early and state up lifting

--- src/component/home.js
+++ src/component/home.js
@@ -1,18 +1,41 @@
+import React from 'react';
 import Hero from './hero-section';
 import Posts from './posts';
 import Tags from './tags';
-import '../styles/home.css'
+import Pagination from './pagination';
+import '../styles/home.css';
+import { ARTICLES_URL } from '../utils/constant';
 
-function Home() {
-  return (
-    <>
-      <Hero />
-      <div className="side-side-flex">
-        <Posts />
+class Home extends React.Component {
+  state = {
+    articles: null,
+    totalArticles: 0,
+    articlesPerPage: 10,
+  };
+
+  componentDidMount() {
+    fetch(ARTICLES_URL + `/?limit=${this.state.articlesPerPage}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          articles: data.articles,
+          totalArticles: data.articlesCount,
+        });
+      });
+  }
+  render() {
+    return (
+      <>
+        <Hero />
+        <Posts articles={this.state.articles} />
         <Tags />
-      </div>
-    </>
-  );
+        <Pagination
+          totalArticles={this.state.totalArticles}
+          articlesPerPage={this.state.articlesPerPage}
+        />
+      </>
+    );
+  }
 }
 
 export default Home;
---
+++ src/component/pagination.js
@@ -0,0 +1,22 @@
+import '../styles/pagination.css';
+
+function Pagination() {
+  return (
+    <div className="pagination">
+      <p>Previous</p>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+      <span>4</span>
+      <span>5</span>
+      <span>6</span>
+      <span>7</span>
+      <span>8</span>
+      <span>9</span>
+      <span>10</span>
+      <p>Next</p>
+    </div>
+  );
+}
+
+export default Pagination;
