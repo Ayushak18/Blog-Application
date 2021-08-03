444041e09fe5ec1681ed3094dc53dc2bc3ebfe1c
Ayushak18 <aayushkamboj7@gmail.com>
2021-08-04 00:09:57 +0530

    New article page UI

--- src/component/newArticle.js
+++ src/component/newArticle.js
@@ -1,5 +1,18 @@
+import '../styles/newArticle.css';
+
 function NewArticle() {
-  return <h1>From NewArticle</h1>;
+  return (
+    <div className="newArticle">
+      <h1>Create new article</h1>
+      <form>
+        <input type="text" placeholder="Title" name="title" />
+        <input type="text" placeholder="Description" name="descriptions" />
+        <textarea cols="30" rows="10" placeholder="Body"></textarea>
+        {/* <input type="text" placeholder="Tags" /> */}
+        <input className="submit-button" type="submit" value="Add Article" />
+      </form>
+    </div>
+  );
 }
 
 export default NewArticle;
