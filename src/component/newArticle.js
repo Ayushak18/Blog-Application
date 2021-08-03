dd1df8f31fd0eb8e3b2fcd63ba0f8f90dfad71ae
Ayushak18 <aayushkamboj7@gmail.com>
2021-08-04 03:14:52 +0530

    Adding new article

--- src/component/newArticle.js
+++ src/component/newArticle.js
@@ -1,18 +1,43 @@
 import '../styles/newArticle.css';
+import { ARTICLES_URL } from '../utils/constant';
+import { withRouter } from 'react-router-dom';
 
-function NewArticle() {
+function NewArticle(props) {
+  let handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    let formData = {
+      article: {
+        title: event.target.title.value,
+        description: event.target.description.value,
+        body: event.target.body.value,
+        tagList: event.target.tagList.value.split(','),
+      },
+    };
+    fetch(ARTICLES_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Token ${localStorage.app_user}`,
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => props.history.push('/'));
+  };
+
   return (
     <div className="newArticle">
       <h1>Create new article</h1>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <input type="text" placeholder="Title" name="title" />
-        <input type="text" placeholder="Description" name="descriptions" />
-        <textarea cols="30" rows="10" placeholder="Body"></textarea>
-        {/* <input type="text" placeholder="Tags" /> */}
+        <input type="text" placeholder="Description" name="description" />
+        <textarea cols="30" rows="10" placeholder="Body" name="body"></textarea>
+        <input type="text" placeholder="Tags" name="tagList" />
         <input className="submit-button" type="submit" value="Add Article" />
       </form>
     </div>
   );
 }
 
-export default NewArticle;
+export default withRouter(NewArticle);
