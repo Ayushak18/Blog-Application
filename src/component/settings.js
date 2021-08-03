eb4c19d9448c49002721980061c9697b7d948d3a
Ayushak18 <aayushkamboj7@gmail.com>
2021-08-04 00:24:39 +0530

    Settings UI

--- src/component/settings.js
+++ src/component/settings.js
@@ -1,5 +1,23 @@
+import '../styles/settings.css';
+
 function Settings() {
-  return <h1>From Settings</h1>;
+  return (
+    <div className="settings-page">
+      <h1>Your Settings</h1>
+      <form>
+        <input type="text" placeholder="URL of Profile Picture" />
+        <input type="text" placeholder="Name" />
+        <textarea
+          cols="30"
+          rows="10"
+          placeholder="Short bio about you"
+        ></textarea>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="New Password" />
+        <input type="submit" value="Submit" className="submit-button" />
+      </form>
+    </div>
+  );
 }
 
 export default Settings;
